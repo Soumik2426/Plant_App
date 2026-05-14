e3d09dbe29725765a2674518b9f9e015c4cc4f41
Soumik2426 <soumikmaity2426@gmail.com>
2026-05-14 14:16:29 +0530

    Added the App.test.tsx file

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -6,6 +6,23 @@ import React from 'react';
 import ReactTestRenderer from 'react-test-renderer';
 import App from '../App';
 
+jest.mock('@react-navigation/native', () => ({
+  NavigationContainer: ({ children }) => children,
+}));
+
+jest.mock('../navigation/AppNavigator', () => {
+  const { createElement } = require('react');
+  const { View, Text } = require('react-native');
+
+  return function MockAppNavigator() {
+    return createElement(
+      View,
+      null,
+      createElement(Text, null, 'MockAppNavigator')
+    );
+  };
+});
+
 test('renders correctly', async () => {
   await ReactTestRenderer.act(() => {
     ReactTestRenderer.create(<App />);
